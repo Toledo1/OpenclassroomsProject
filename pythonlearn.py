6c4d2b5889c08ffeef6d96f05b6db8c90df54c0e
Mbiethieu Cezar <mbiethieucezar@gmail.com>
2022-07-14 01:28:04 +0200

    Ajout des fichiers index, style et pythonlearn à la base

---
+++ pythonlearn.py
@@ -0,0 +1,15 @@
+import numpy as np
+liste = ["CZR", "toledo"]
+
+print(liste[0])
+liste.append('mbiethieu')
+for i in liste:
+    print(i)
+
+
+nouvelle_campagne = {
+    "responsable_de_campagne": "Jeanne d'Arc",
+    "nom_de_campagne": "Campagne nous aimons les chiens",
+    "date_de_début": "01/01/2020",
+    "influenceurs_importants": ["@MonAmourDeChien", "@MeilleuresFriandisesPourChiens"]
+}
